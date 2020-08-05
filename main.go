c65846aed2edb892d74f774d25d72755bdb4f5be
川岡優太 <kawaokayuta@vinaka.local>
2020-08-05 16:04:32 +0900

    learn variable

--- main.go
+++ main.go
@@ -4,6 +4,17 @@ import (
 	"fmt"
 )
 
-func main() {
-	fmt.Println("Hello vinaka")
+
+	// 変数の定義方法三つ
+	// var name string = "vinaka"
+	// var name = "vinaka"
+	func main() {
+    name := "vinaka"
+    
+    fmt.Printf("hello %v\n", name)
+    fmt.Printf("hello %v again!", name)
 }
+
+// Printfは改行なし
+// Printlnは改行あり
+
